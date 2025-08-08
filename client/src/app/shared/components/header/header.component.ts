d436239b3bf99527ebe84729719ee97b0ff4d709
georgi712 <152554904+georgi712@users.noreply.github.com>
2025-08-08 11:35:32 +0300

    Dynamic header

--- client/src/app/shared/components/header/header.component.ts
+++ client/src/app/shared/components/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services';
+import { Observable } from 'rxjs';
+import { User } from '@angular/fire/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  private authService = inject(AuthService);
+  user$ = this.authService.currentUser$;
+  logout(): void {
+    this.authService.logout();
+  }
 }
