40be8b0e6098bf20c285219db93eabd74248229e
georgi712 <152554904+georgi712@users.noreply.github.com>
2025-08-13 18:07:03 +0300

    Navigate to home page after logout

--- client/src/app/shared/components/header/header.component.ts
+++ client/src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services';
 import { CommonModule } from '@angular/common';
 
@@ -12,6 +12,7 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
   private authService = inject(AuthService);
+  private router = inject(Router)
 
   user = this.authService.user;
   isLoggedIn = computed(() => !!this.user());
@@ -19,5 +20,6 @@ export class HeaderComponent {
 
   logout(): void {
     this.authService.logout();
+    this.router.navigate(['/'])
   }
 }
