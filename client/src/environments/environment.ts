f6b359e2062a27d983ada9dbaf61eb3f8eec579c
georgi712 <152554904+georgi712@users.noreply.github.com>
2025-08-13 22:53:36 +0300

    Add environment file and deployment set up

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+    apiKey: "AIzaSyBLkB1jgycYSs327mfqnbjS8JbGdikmSE8",
+    authDomain: "book-buddy-18f31.firebaseapp.com",
+    projectId: "book-buddy-18f31",
+    storageBucket: "book-buddy-18f31.firebasestorage.app",
+    messagingSenderId: "470759388426",
+    appId: "1:470759388426:web:8a88894f9e6c9a71e076f3",
+    measurementId: "G-2P8B4W2KE5" 
+    }
+};
